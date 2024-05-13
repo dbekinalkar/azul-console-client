16398f4757adc789494cd46dd74e6dd15a01ac23
Daniyal Bekinalkar <daniyal.bekinalkar@gmail.com>
2024-05-13 02:11:40 -0500

    Setup client to communicate with websocket connection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+import asyncio
+import websockets
 
 def parse_args() -> argparse.Namespace:
     parser: argparse.ArgumentParser = argparse.ArgumentParser(description="Client to connect to Azul server.")
@@ -7,10 +9,29 @@ def parse_args() -> argparse.Namespace:
     args: argparse.Namespace = parser.parse_args()
     return args
 
-def main() -> None:
+async def main() -> None:
     args: argparse.Namespace = parse_args()
 
-    print(f'Connecting to {args.host}:{args.port}')
+    uri: str = f"ws://{args.host}:{args.port}"
+
+    print(f'Connecting to {uri}')
+
+    ws: websockets.WebSocketClientProtocol
+    async with websockets.connect(uri) as ws:
+        while True:
+            message: str = input()
+            if message == "quit":
+                break
+                
+            await ws.send(message)
+
+            receipt: str = await ws.recv()
+
+            print(f"Received from server: {receipt}")
+            
+
+    print("Connection closed")
+
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
